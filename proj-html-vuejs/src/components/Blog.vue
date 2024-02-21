cda54791d392a3e533c1cff7e504dbe007b1bba0
Anna <annadalex@icloud.com>
2024-02-21 01:01:18 +0100

    INSERIMENTO ULTIMO SVG

--- proj-html-vuejs/src/components/Blog.vue
+++ proj-html-vuejs/src/components/Blog.vue
@@ -11,12 +11,28 @@ export default{
 </script>
 
 <template>
-    <div>
+    <div id="blog">
+        <svg xmlns="http://www.w3.org/2000/svg"
+     class="fusion-big-triangle-candy"
+     version="1.1"
+     width="100%"
+     height="100"
+     viewBox="0 0 100 100"
+     preserveAspectRatio="none"
+     style="display: block; fill:#FFFFFF; padding:0;">
+  <path d="M0 100 L50 2 L100 100 Z" transform="rotate(180, 50, 50)"/>
+</svg>
+       
+        
        
     </div>
   
 </template>
 
 <style lang="scss" scoped>
+#blog{
+    background-color: #f5f5f5;
+}
+
 
 </style>
--- proj-html-vuejs/src/components/Services.vue
+++ proj-html-vuejs/src/components/Services.vue
@@ -14,7 +14,8 @@ export default{
 <template>
     <div id="services">
         <div class="triangle">
-          <svg xmlns="http://www.w3.org/2000/svg" class="fusion-big-triangle-candy" version="1.1" width="100%" height="100" viewBox="0 0 100 100" preserveAspectRatio="none" style="fill:#ffffff;padding:0;"><path d="M0 100 L50 2 L100 100 Z"/></svg>  
+          <svg xmlns="http://www.w3.org/2000/svg" class="fusion-big-triangle-candy" version="1.1" width="100%" height="100" viewBox="0 0 100 100" preserveAspectRatio="none" 
+          style="fill:#ffffff;padding:0;"><path d="M0 100 L50 2 L100 100 Z"/></svg>  
         </div>
 
         <div class="d-flex flex-column align-items-center">
